c8bde66b3e77e6b312a09119c78ff79c933efcab
duongchikienk63 <95597617+duongchikienk63@users.noreply.github.com>
2023-02-14 09:55:39 +0700

    code

---
+++ public/init-firebase.js
@@ -0,0 +1,13 @@
+// TODO: Replace the following with your app's Firebase project configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyCEUgbFgDEZ7WzM1uE3RJp2DAuM2CDFJZo",
+    authDomain: "fireauth-248a0.firebaseapp.com",
+    projectId: "fireauth-248a0",
+    storageBucket: "fireauth-248a0.appspot.com",
+    messagingSenderId: "12423736286",
+    appId: "1:12423736286:web:c94a1f2b16d95baa5ecb4f",
+    measurementId: "G-SEDFEDX3JE"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
